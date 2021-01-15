2446bc30d0bf76708f97ecc32bad17a48a9d4423
Tyler Mace <tylerdmace@gmail.com>
2021-01-15 13:02:15 -0700

    Labs.py additions

--- templates/labs/labs.py
+++ templates/labs/labs.py
@@ -1,1 +1,10 @@
 # labs.py
+#
+# Description: This file is required to make a cadCAD project useable with the
+#              BlockScience Labs platform.
+#
+# Usage: Import any files that define Experiment and/or Configuration objects.
+#
+
+import .model/config
+import .model/run
